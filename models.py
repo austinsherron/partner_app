a229eef113f85f836dba8c09ca1c0837e352ec78
Bert Austin Sherron <mail@austinsherron.com>
2016-07-28 13:11:41 -0700

    Update Model Formatting
    Update model formatting.

--- models.py
+++ models.py
@@ -17,16 +17,16 @@ from google.appengine.ext import ndb
 
 
 class Assignment(ndb.Model):
-	number = ndb.IntegerProperty()
-	fade_in_date = ndb.DateTimeProperty()
-	due_date = ndb.DateTimeProperty()
-	close_date = ndb.DateTimeProperty()
+	number         = ndb.IntegerProperty()
+	fade_in_date   = ndb.DateTimeProperty()
+	due_date       = ndb.DateTimeProperty()
+	close_date     = ndb.DateTimeProperty()
 	eval_open_date = ndb.DateTimeProperty()
-	eval_date = ndb.DateTimeProperty()
-	fade_out_date = ndb.DateTimeProperty()
-	quarter = ndb.IntegerProperty()
-	year = ndb.IntegerProperty()
-	current = ndb.GenericProperty()
+	eval_date      = ndb.DateTimeProperty()
+	fade_out_date  = ndb.DateTimeProperty()
+	quarter        = ndb.IntegerProperty()
+	year           = ndb.IntegerProperty()
+	current        = ndb.GenericProperty()
 
 
 ################################################################################
@@ -41,8 +41,8 @@ class Assignment(ndb.Model):
 
 class Instructor(ndb.Model):
 	ucinetid = ndb.StringProperty()
-	name = ndb.StringProperty()
-	email = ndb.StringProperty()
+	name     = ndb.StringProperty()
+	email    = ndb.StringProperty()
 
 
 ################################################################################
@@ -56,22 +56,22 @@ class Instructor(ndb.Model):
 
 
 class Student(ndb.Model):
-	ucinetid = ndb.StringProperty()
-	studentid = ndb.IntegerProperty()
-	first_name = ndb.StringProperty()
-	last_name = ndb.StringProperty()
-	preferred_name = ndb.StringProperty()
-	email = ndb.StringProperty()
-	lab = ndb.IntegerProperty()
-	quarter = ndb.IntegerProperty()
-	year = ndb.IntegerProperty()
-	active = ndb.GenericProperty()
-	bio = ndb.TextProperty()
+	ucinetid            = ndb.StringProperty()
+	studentid           = ndb.IntegerProperty()
+	first_name          = ndb.StringProperty()
+	last_name           = ndb.StringProperty()
+	preferred_name      = ndb.StringProperty()
+	email               = ndb.StringProperty()
+	lab                 = ndb.IntegerProperty()
+	quarter             = ndb.IntegerProperty()
+	year                = ndb.IntegerProperty()
+	active              = ndb.GenericProperty()
+	bio                 = ndb.TextProperty()
 	programming_ability = ndb.StringProperty()
-	avatar = ndb.BlobProperty()
-	phone_number = ndb.StringProperty()
-	availability = ndb.TextProperty()
-	created = ndb.DateTimeProperty(auto_now_add=True)
+	avatar              = ndb.BlobProperty()
+	phone_number        = ndb.StringProperty()
+	availability        = ndb.TextProperty()
+	created             = ndb.DateTimeProperty(auto_now_add=True)
 
 
 ################################################################################
@@ -85,12 +85,12 @@ class Student(ndb.Model):
 
 
 class Invitation(ndb.Model):
-	invitor = ndb.KeyProperty(kind=Student)
-	invitee = ndb.KeyProperty(kind=Student)
+	invitor           = ndb.KeyProperty(kind=Student)
+	invitee           = ndb.KeyProperty(kind=Student)
 	assignment_number = ndb.IntegerProperty()
-	active = ndb.GenericProperty()
-	accepted = ndb.BooleanProperty(default=False)
-	created = ndb.DateTimeProperty(auto_now_add=True)
+	active            = ndb.GenericProperty()
+	accepted          = ndb.BooleanProperty(default=False)
+	created           = ndb.DateTimeProperty(auto_now_add=True)
 
 
 ################################################################################
@@ -104,15 +104,15 @@ class Invitation(ndb.Model):
 
 
 class Partnership(ndb.Model):
-        members = ndb.KeyProperty(repeated=True)
+        members           = ndb.KeyProperty(repeated=True)
 	assignment_number = ndb.IntegerProperty()
-	active = ndb.GenericProperty()
-	quarter = ndb.IntegerProperty()
-	year = ndb.IntegerProperty()
-	notes = ndb.TextProperty(default='')
-	created = ndb.DateTimeProperty(auto_now_add=True)
-        cancelled = ndb.KeyProperty(repeated=True)
-        solo = ndb.ComputedProperty(lambda p: len(p.members) == 1)
+	active            = ndb.GenericProperty()
+	quarter           = ndb.IntegerProperty()
+	year              = ndb.IntegerProperty()
+	notes             = ndb.TextProperty(default='')
+	created           = ndb.DateTimeProperty(auto_now_add=True)
+        cancelled         = ndb.KeyProperty(repeated=True)
+        solo              = ndb.ComputedProperty(lambda p: len(p.members) == 1)
 	
 
 ################################################################################
@@ -126,14 +126,14 @@ class Partnership(ndb.Model):
 
 
 class Evaluation(ndb.Model):
-	evaluator = ndb.KeyProperty(kind=Student)
-	evaluatee = ndb.KeyProperty(kind=Student)
+	evaluator         = ndb.KeyProperty(kind=Student)
+	evaluatee         = ndb.KeyProperty(kind=Student)
 	assignment_number = ndb.IntegerProperty()
-	responses = ndb.StringProperty(repeated=True)
-	year = ndb.IntegerProperty()
-	quarter = ndb.IntegerProperty()
-	active = ndb.GenericProperty()
-	created = ndb.DateTimeProperty(auto_now_add=True)
+	responses         = ndb.StringProperty(repeated=True)
+	year              = ndb.IntegerProperty()
+	quarter           = ndb.IntegerProperty()
+	active            = ndb.GenericProperty()
+	created           = ndb.DateTimeProperty(auto_now_add=True)
 
 
 ################################################################################
@@ -147,12 +147,12 @@ class Evaluation(ndb.Model):
 
 
 class Setting(ndb.Model):
-	year = ndb.IntegerProperty()
-	quarter = ndb.IntegerProperty()
-	num_labs = ndb.IntegerProperty()
-	repeat_partners = ndb.BooleanProperty(default=False)
+	year                   = ndb.IntegerProperty()
+	quarter                = ndb.IntegerProperty()
+	num_labs               = ndb.IntegerProperty()
+	repeat_partners        = ndb.BooleanProperty(default=False)
 	cross_section_partners = ndb.BooleanProperty(default=False)
-        group_max = ndb.IntegerProperty()
+        group_max              = ndb.IntegerProperty()
 	
 
 ################################################################################
